2724ca7d7d9856d1ba85e965f906c0cfbc1a090e
Benjamin Du <longendu@yahoo.com>
2020-01-29 19:44:03 -0800

    1. improve msg; 2. better string

--- dsutil/poetry.py
+++ dsutil/poetry.py
@@ -10,9 +10,9 @@ import subprocess as sp
 import toml
 from loguru import logger
 from .filesystem import update_file
-DIST = 'dist'
-README = 'readme.md'
-TOML = 'pyproject.toml'
+DIST = "dist"
+README = "readme.md"
+TOML = "pyproject.toml"
 
 
 def _project_dir() -> Path:
@@ -25,7 +25,7 @@ def _project_dir() -> Path:
             return path
         path = path.parent
     raise RuntimeError(
-        f'The current work directory {Path.cwd()} is not a (subdirectory of a) Python Poetry project.'
+        f"The current work directory {Path.cwd()} is not a (subdirectory of a) Python Poetry project."
     )
 
 
@@ -34,14 +34,14 @@ def _project_name(proj_dir: Path) -> str:
     :param proj_dir: The root directory of the Poetry project.
     :return: The name of the project.
     """
-    return toml.load(proj_dir / TOML)['tool']['poetry']['name']
+    return toml.load(proj_dir / TOML)["tool"]["poetry"]["name"]
 
 
 def _project_version(proj_dir: Path) -> str:
     """Get the version of the project.
     :param proj_dir: The root directory of the Poetry project.
     """
-    return toml.load(proj_dir / TOML)['tool']['poetry']['version']
+    return toml.load(proj_dir / TOML)["tool"]["poetry"]["version"]
 
 
 def _update_version_readme(ver: str, proj_dir: Path) -> None:
@@ -49,7 +49,7 @@ def _update_version_readme(ver: str, proj_dir: Path) -> None:
     :param ver: The new version.
     :param proj_dir: The root directory of the Poetry project.
     """
-    update_file(proj_dir / README, rf'\d+\.\d+\.\d+', f'{ver}')
+    update_file(proj_dir / README, rf"\d+\.\d+\.\d+", f"{ver}")
 
 
 def _update_version_toml(ver: str, proj_dir: Path) -> None:
@@ -58,7 +58,7 @@ def _update_version_toml(ver: str, proj_dir: Path) -> None:
     :param proj_dir: The root directory of the Poetry project.
     """
     update_file(
-        proj_dir / TOML, r'version = .\d+\.\d+\.\d+.', f'version = "{ver}"'
+        proj_dir / TOML, r"version = .\d+\.\d+\.\d+.", f'version = "{ver}"'
     )
 
 
@@ -73,7 +73,7 @@ def _update_version_init(ver: str, proj_dir: Path) -> None:
             path = Path(subdir, file)
             if path.suffix == ".py":
                 update_file(
-                    path, r'__version__ = .\d+\.\d+\.\d+.',
+                    path, r"__version__ = .\d+\.\d+\.\d+.",
                     f'__version__ = "{ver}"'
                 )
 
@@ -87,7 +87,7 @@ def _update_version(ver: str, proj_dir: Path) -> None:
         _update_version_init(ver=ver, proj_dir=proj_dir)
         _update_version_toml(ver, proj_dir=proj_dir)
         _update_version_readme(ver=ver, proj_dir=proj_dir)
-        sp.run(['git', 'diff'], check=True)
+        sp.run(["git", "diff"], check=True)
 
 
 def _list_version(proj_dir: Path):
@@ -95,7 +95,7 @@ def _list_version(proj_dir: Path):
 
 
 def version(
-    ver: str = '',
+    ver: str = "",
     proj_dir: Path = None,
 ):
     """List or update the version of the package.
@@ -114,13 +114,13 @@ def version(
 def _format_code(inplace: bool = False, proj_dir: Path = None):
     if proj_dir is None:
         proj_dir = _project_dir()
-    cmd = ['yapf', '-r']
+    cmd = ["yapf", "-r"]
     if inplace:
-        cmd.append('-i')
-        logger.info('Formatting code...')
+        cmd.append("-i")
+        logger.info("Formatting code...")
     else:
-        cmd.append('-d')
-        logger.info('Checking code formatting...')
+        cmd.append("-d")
+        logger.info("Checking code formatting...")
     pkg = _project_name(proj_dir)
     cmd.append(str(proj_dir / pkg))
     proc = sp.run(cmd, check=False, stdout=sp.PIPE)
@@ -128,7 +128,7 @@ def _format_code(inplace: bool = False, proj_dir: Path = None):
         print(proc.stdout.decode())
         sys.stdout.flush()
         sys.stderr.flush()
-        logger.warning('Please format the code (yapf -ir .)!')
+        logger.warning(f"Please format the code (yapf -ir {pkg})!")
 
 
 def build_package(proj_dir: Path = None) -> None:
@@ -142,8 +142,10 @@ def build_package(proj_dir: Path = None) -> None:
         shutil.rmtree(DIST)
     pkg = _project_name(proj_dir)
     try:
-        logger.info(f'Checking code for errors (pylint -E {pkg}) ...')
-        with open(os.devnull, 'w') as devnull:
+        logger.info(
+            f"Checking code for errors (.venv/bin/python -m pylint -E {pkg}) ..."
+        )
+        with open(os.devnull, "w") as devnull:
             sp.run(
                 f"cd {proj_dir} && pylint -E {pkg}",
                 shell=True,
@@ -151,10 +153,10 @@ def build_package(proj_dir: Path = None) -> None:
                 stderr=devnull
             )
     except sp.CalledProcessError:
-        logger.error('Please fix errors in code before building the package!')
+        logger.error("Please fix errors in code before building the package!")
         return
     _format_code(proj_dir=proj_dir)
-    logger.info('Building the package...')
+    logger.info("Building the package...")
     sp.run(
         f"cd '{proj_dir}' && poetry env use python3 && poetry build",
         shell=True,
@@ -168,13 +170,13 @@ def install_package(options: List[str] = (), proj_dir: Path = None):
     """
     if proj_dir is None:
         proj_dir = _project_dir()
-    pkg = list(proj_dir.glob('dist/*.whl'))
+    pkg = list(proj_dir.glob("dist/*.whl"))
     if not pkg:
-        logger.error('No built package is found!')
+        logger.error("No built package is found!")
         return
     if len(pkg) > 1:
-        logger.error('Multiple built packages are found!')
+        logger.error("Multiple built packages are found!")
         return
-    cmd = ['pip3', 'install', '--user', '--upgrade', pkg[0]]
+    cmd = ["pip3", "install", "--user", "--upgrade", pkg[0]]
     cmd.extend(options)
     sp.run(cmd, check=True)
