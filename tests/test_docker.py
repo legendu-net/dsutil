17095a8aa045336ec60f2a78ce4c33dbe7e2c082
Benjamin Du <longendu@yahoo.com>
2020-09-13 22:11:33 -0700

    skip test on macos

--- tests/test_docker.py
+++ tests/test_docker.py
@@ -1,12 +1,15 @@
 """Testing the module dsutil.docker.
 """
+import sys
+import pytest
 import dsutil
-from pathlib import Path
 
 
+@pytest.mark.skipif(sys.platform == "darwin", reason="Skip test for Mac OS")
 def test_images():
     dsutil.docker.images()
 
 
+@pytest.mark.skipif(sys.platform == "darwin", reason="Skip test for Mac OS")
 def test_remove_images():
     dsutil.docker.remove_images(name="nimade")
