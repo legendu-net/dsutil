515c22f98c75f9eb62b2f1856b0da5821738e1e0
Benjamin Du <longendu@yahoo.com>
2020-10-18 11:52:35 -0700

    update tests

--- tests/test_docker.py
+++ tests/test_docker.py
@@ -1,17 +1,17 @@
 """Testing the module dsutil.docker.
 """
 import shutil
-#import pytest
+import pytest
 import dsutil
 
 
-#@pytest.mark.skipif(sys.platform == "darwin", reason="Skip test for Mac OS")
+@pytest.mark.skipif(sys.platform == "darwin", reason="Skip test for Mac OS")
 def test_images():
     if shutil.which("docker"):
         dsutil.docker.images()
 
 
-#@pytest.mark.skipif(sys.platform == "darwin", reason="Skip test for Mac OS")
+@pytest.mark.skipif(sys.platform == "darwin", reason="Skip test for Mac OS")
 def test_remove_images():
     if shutil.which("docker"):
         dsutil.docker.remove_images(name="nimade")
