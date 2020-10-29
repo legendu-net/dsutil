56cf76ad515628217183db44873811ac328fdf5b
Benjamin Du <longendu@yahoo.com>
2020-10-29 11:55:34 -0700

    fix issue

--- dsutil/jupyter.py
+++ dsutil/jupyter.py
@@ -4,10 +4,10 @@
 import os
 from typing import Union, Dict
 from pathlib import Path
-import subprocess as sp
 import tempfile
 import nbformat
 from loguru import logger
+from nbconvert import HTMLExporter
 from yapf.yapflib.yapf_api import FormatCode
 HOME = Path.home()
 
@@ -61,16 +61,13 @@ def nbconvert_notebooks(root_dir: Union[str, Path], cache: bool = False) -> None
     if isinstance(root_dir, str):
         root_dir = Path(root_dir)
     notebooks = root_dir.glob("**/*.ipynb")
+    exporter = HTMLExporter()
     for notebook in notebooks:
-        html = notebook.with_suffix(".html")
         if cache and html.is_file(
         ) and html.stat().st_mtime >= notebook.stat().st_mtime:
             continue
-        sp.run(
-            f"jupyter nbconvert {notebook} --to html --output {html}",
-            shell=True,
-            check=True
-        )
+        html, _ = exporter.from_notebook_node(nbformat.read(notebook, as_version=4))
+        notebook.with_suffix(".html").write_text(html)
 
 
 def _format_notebook(path: Path, style_file: str):
