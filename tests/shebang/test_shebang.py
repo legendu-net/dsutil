4b6771df524f7dbe3578a6d70cbbea1a4c1279e8
Benjamin Du <longendu@yahoo.com>
2020-11-09 13:17:37 -0800

    unit test

--- tests/shebang/test_shebang.py
+++ tests/shebang/test_shebang.py
@@ -1,2 +1,8 @@
+from pathlib import Path
+import dsutil.shebang
+BASE_DIR = Path(__file__).parent
+
+
 def test_shebang():
-    ""
+    dsutil.shebang.update_shebang(BASE_DIR / "script_dir", "#!/usr/bin/env python3")
+    
