b2ea2b5780199e9e65c3adbeb39f71a0e8e42372
Benjamin Du <longendu@yahoo.com>
2020-11-09 13:56:22 -0800

    skip test_shebang on windows

--- tests/shebang/test_shebang.py
+++ tests/shebang/test_shebang.py
@@ -1,7 +1,10 @@
+import sys
 from pathlib import Path
+import pytest
 import dsutil.shebang
 BASE_DIR = Path(__file__).parent
 
 
+@pytest.mark.skipif(sys.platform == "win32", reason="Skip test on Windows")
 def test_shebang():
     dsutil.shebang.update_shebang(BASE_DIR / "script_dir", "#!/usr/bin/env python3")
