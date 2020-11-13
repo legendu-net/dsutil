d78569027e93fc09955c25c25a94c664277c8f7a
Benjamin Du <longendu@yahoo.com>
2020-11-12 20:48:39 -0800

    improve the docker module

--- tests/docker/test_docker.py
+++ tests/docker/test_docker.py
@@ -2,8 +2,10 @@
 """
 import sys
 import shutil
+from pathlib import Path
 import pytest
 import dsutil
+BASE_DIR = Path(__file__).parent
 
 
 @pytest.mark.skipif(sys.platform == "darwin", reason="Skip test for Mac OS")
@@ -16,3 +18,9 @@ def test_images():
 def test_remove_images():
     if shutil.which("docker"):
         dsutil.docker.remove_images(name="nimade")
+
+
+def test_copy_ssh():
+    builder = dsutil.docker.DockerImage(git_url="")
+    builder.path = BASE_DIR
+    builder._copy_ssh("ssh")
