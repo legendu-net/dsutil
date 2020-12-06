82cfbda18a28e050b05b0eb2aba47e3d01127c69
Benjamin Du <longendu@yahoo.com>
2020-12-05 22:23:08 -0800

    add a new sql moduel

---
+++ dsutil/sql.py
@@ -0,0 +1,19 @@
+from typing import Union
+from pathlib import Path
+import sqlparse
+
+
+def format(path: Union[Path, str]):
+    if isinstance(path, str):
+        path = Path(path)
+    queries = path.read_text()
+    query = sqlparse.format(query,
+        keyword_case="upper",
+        identifier_case="lower",
+        strip_comments=False,
+        reindent=True,
+        indent_width=2
+    )
+    path.write_text(query)
+    cmd = f"pg_format --function-case 1 --type-case 3 --inplace {path}"
+    sp.run(cmd, shell=True, check=True)
