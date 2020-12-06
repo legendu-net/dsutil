f0048bc0951b7a28491cf441e9043a135bf8e25a
Benjamin Du <longendu@yahoo.com>
2020-12-06 12:29:08 -0800

    add unit test

--- tests/docker/test_docker.py
+++ tests/docker/test_docker.py
@@ -9,12 +9,6 @@ BASE_DIR = Path(__file__).parent
 
 
 @pytest.mark.skipif(sys.platform == "darwin", reason="Skip test for Mac OS")
-def test_images():
-    if shutil.which("docker"):
-        dsutil.docker.images()
-
-
-@pytest.mark.skipif(sys.platform == "darwin", reason="Skip test for Mac OS")
 def test_remove_images():
     if shutil.which("docker"):
         dsutil.docker.remove_images(name="nimade")
@@ -24,3 +18,14 @@ def test_copy_ssh():
     builder = dsutil.docker.DockerImage(git_url="")
     builder.path = BASE_DIR
     builder._copy_ssh("ssh")
+
+
+@pytest.mark.skipif(sys.platform == "darwin", reason="Skip test for Mac OS")
+def test_DockerImageBuilder():
+    if not shutil.which("docker"):
+        return
+    images = [
+        "https://github.com/dclong/docker-python-portable.git",
+        ]
+    builder = dsutil.docker.DockerImageBuilder(images, branch="master")
+    builder.build(tag_build="unittest")
