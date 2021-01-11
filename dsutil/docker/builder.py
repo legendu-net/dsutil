d7acc26c07cdb23f48a6ddf0cb6f28ff27521cb0
Ben Chuanlong Du <legendu.net@outlook.com>
2021-01-11 00:20:25 -0800

    fix issue

--- dsutil/docker/builder.py
+++ dsutil/docker/builder.py
@@ -311,7 +311,7 @@ class DockerImageBuilder:
             print("git_url: ", git_url)
             deps: Sequence[DockerImage] = DockerImage(
                 git_url=git_url, branch=self.branch
-            ).get_deps()
+            ).get_deps({})
             print("length of deps", len(deps))
             print("deps: ", deps)
             for idx in range(1, len(deps)):
