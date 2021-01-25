6b359c372e3094ade1fca8f74bc6cebea57ac3e0
Ben Chuanlong Du <legendu.net@outlook.com>
2021-01-24 19:48:44 -0800

    logging res

--- dsutil/docker/builder.py
+++ dsutil/docker/builder.py
@@ -528,6 +528,7 @@ class DockerImageBuilder:
             )
         if not remove:
             return
+        logger.debug("res: {}\n", res)
         # remove images associate with node
         client = docker.from_env()
         for image_name, tag, *_ in res:
