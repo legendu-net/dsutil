521b3b4981678af607677306830be0787ea77d54
Ben Chuanlong Du <legendu.net@outlook.com>
2021-03-17 09:23:43 -0700

    format code

--- tests/hadoop/log/test_logf.py
+++ tests/hadoop/log/test_logf.py
@@ -7,7 +7,7 @@ BASE_DIR = Path(__file__).parent
 
 
 def test_main():
-    args = dsutil.hadoop.logf.parse_args([
-        "filter", str(BASE_DIR / "application_1611634725250_1347938")
-    ])
+    args = dsutil.hadoop.logf.parse_args(
+        ["filter", str(BASE_DIR / "application_1611634725250_1347938")]
+    )
     dsutil.hadoop.logf.main(args)
