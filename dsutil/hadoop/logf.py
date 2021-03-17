798681c4b285facae84c7c3ca7c4cc8a2763db34
Ben Chuanlong Du <legendu.net@outlook.com>
2021-03-17 12:48:11 -0700

    single quotes to double quotes

--- dsutil/hadoop/logf.py
+++ dsutil/hadoop/logf.py
@@ -6,7 +6,7 @@ from argparse import ArgumentParser, Namespace
 import subprocess as sp
 from .log import LogFilter
 
-YARN = '/apache/hadoop/bin/yarn'
+YARN = "/apache/hadoop/bin/yarn"
 
 
 def filter_(args):
@@ -27,12 +27,12 @@ def filter_(args):
 def _format_app_id(app_id: str):
     app_id = app_id.lower()
     # support old Hive job id of the format job_123456789_123456
-    app_id = re.sub('^job_', 'application_', app_id)
+    app_id = re.sub("^job_", "application_", app_id)
     # support job id of the format _123456789_123456
-    app_id = re.sub('^_', 'application_', app_id)
+    app_id = re.sub("^_", "application_", app_id)
     # support job id of the format 123456789_123456
-    if not app_id.startswith('application_'):
-        app_id = 'application_' + app_id
+    if not app_id.startswith("application_"):
+        app_id = "application_" + app_id
     return app_id
 
 
@@ -43,10 +43,10 @@ def fetch(args):
     """
     app_id = _format_app_id(args.app_id)
     output = args.output if args.output else app_id
-    cmd = [YARN, 'logs', '-applicationId', app_id]
+    cmd = [YARN, "logs", "-applicationId", app_id]
     if args.user:
-        cmd = cmd + ['-appOwner', args.user]
-    with open(output, 'w', encoding='utf-8') as fout:
+        cmd = cmd + ["-appOwner", args.user]
+    with open(output, "w", encoding="utf-8") as fout:
         sp.run(cmd, stdout=fout, check=True)
     args.log_file = output
     filter_(args)
@@ -60,8 +60,8 @@ def parse_args(args=None, namespace=None) -> Namespace:
     :param namespace: An inital Namespace object.
     :return: A namespace object containing parsed options.
     """
-    parser = ArgumentParser(description='Spark/Hadoop log utils.')
-    subparsers = parser.add_subparsers(help='Sub commands.')
+    parser = ArgumentParser(description="Spark/Hadoop log utils.")
+    subparsers = parser.add_subparsers(help="Sub commands.")
     _subparser_fetch(subparsers)
     _subparser_filter(subparsers)
     return parser.parse_args(args=args, namespace=namespace)
@@ -69,68 +69,68 @@ def parse_args(args=None, namespace=None) -> Namespace:
 
 def _option_filter(subparser) -> None:
     subparser.add_argument(
-        '-k',
-        '--keywords',
-        nargs='+',
-        dest='keywords',
+        "-k",
+        "--keywords",
+        nargs="+",
+        dest="keywords",
         default=LogFilter.KEYWORDS,
-        help='user-defined keywords to search for in the log file'
+        help="user-defined keywords to search for in the log file"
     )
     subparser.add_argument(
-        '-i',
-        '--ignore-patterns',
-        nargs='+',
-        dest='patterns',
+        "-i",
+        "--ignore-patterns",
+        nargs="+",
+        dest="patterns",
         default=LogFilter.PATTERNS,
         help=
-        'regular expression patterns (date/time and ip by default) to ignore in dedup of filtered lines.'
+        "regular expression patterns (date/time and ip by default) to ignore in dedup of filtered lines."
     )
     subparser.add_argument(
-        '-c',
-        '--context-size',
+        "-c",
+        "--context-size",
         type=int,
-        dest='context_size',
+        dest="context_size",
         default=3,
         help=
-        'number of lines (3 by default) to print before and after the suspicious line.'
+        "number of lines (3 by default) to print before and after the suspicious line."
     )
     subparser.add_argument(
-        '-o',
-        '--output',
-        dest='output',
-        help='path of the output file (containing filtered lines).'
+        "-o",
+        "--output",
+        dest="output",
+        help="path of the output file (containing filtered lines)."
     )
     subparser.add_argument(
-        '-t',
-        '--threshold',
-        dest='threshold',
+        "-t",
+        "--threshold",
+        dest="threshold",
         type=float,
         default=0.5,
-        help='make pattern matching case-sensitive.'
+        help="make pattern matching case-sensitive."
     )
 
 
 def _subparser_fetch(subparsers):
     subparser_fetch = subparsers.add_parser(
-        'fetch', help='fetch the log of a Spark/Hive application.'
+        "fetch", help="fetch the log of a Spark/Hive application."
     )
     subparser_fetch.add_argument(
-        'app_id', help='the ID of the Spark/Hive application whose log is to fetch.'
+        "app_id", help="the ID of the Spark/Hive application whose log is to fetch."
     )
     subparser_fetch.add_argument(
-        '-u',
-        '--user',
-        dest='user',
+        "-u",
+        "--user",
+        dest="user",
         default=None,
-        help='the name of the Spark/Hive application owner.'
+        help="the name of the Spark/Hive application owner."
     )
     subparser_fetch.add_argument(
-        '-m',
-        '--b-marketing-ep-infr',
-        dest='user',
-        action='store_const',
-        const='b_marketing_ep_infr',
-        help='Fetch log using the acount b_marketing_ep_infr.'
+        "-m",
+        "--b-marketing-ep-infr",
+        dest="user",
+        action="store_const",
+        const="b_marketing_ep_infr",
+        help="Fetch log using the acount b_marketing_ep_infr."
     )
     _option_filter(subparser_fetch)
     subparser_fetch.set_defaults(func=fetch)
@@ -138,10 +138,10 @@ def _subparser_fetch(subparsers):
 
 def _subparser_filter(subparsers):
     subparser_filter = subparsers.add_parser(
-        'filter', help='filter key information from a Spark/Hive application log.'
+        "filter", help="filter key information from a Spark/Hive application log."
     )
     subparser_filter.add_argument(
-        'log_file', type=str, help='path of the log file to process'
+        "log_file", type=str, help="path of the log file to process"
     )
     _option_filter(subparser_filter)
     subparser_filter.set_defaults(func=filter_)
@@ -155,5 +155,5 @@ def main(args: Optional[Namespace] = None):
     args.func(args)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
