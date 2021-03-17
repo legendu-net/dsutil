1533c6d9d97ce801f234da6663d3fffec3e6cd7b
Ben Chuanlong Du <legendu.net@outlook.com>
2021-03-17 13:53:58 -0700

    improve test on logf

--- tests/hadoop/log/test_logf.py
+++ tests/hadoop/log/test_logf.py
@@ -7,7 +7,9 @@ BASE_DIR = Path(__file__).parent
 
 
 def test_main():
+    output = BASE_DIR / "log_s"
     args = dsutil.hadoop.logf.parse_args(
-        ["filter", str(BASE_DIR / "application_1611634725250_1347938")]
+        ["filter", str(BASE_DIR / "application_1611634725250_1347938"), "-o", str(output)]
     )
     dsutil.hadoop.logf.main(args)
+    assert output.is_file()
