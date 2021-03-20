ffc64880176db5f8b159fa210f09ff64f18ab75b
Ben Chuanlong Du <legendu.net@outlook.com>
2021-03-20 16:45:35 -0700

    1. threshold -> 0.6; fix regex pattern issue

--- dsutil/hadoop/logf.py
+++ dsutil/hadoop/logf.py
@@ -107,7 +107,7 @@ def _option_filter(subparser) -> None:
         "--threshold",
         dest="threshold",
         type=float,
-        default=0.5,
+        default=0.6,
         help="make pattern matching case-sensitive."
     )
     subparser.add_argument(
