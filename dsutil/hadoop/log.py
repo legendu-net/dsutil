0aab9419b8b48ab3a8e6ec8ccf3ee73fd849cc7a
Ben Chuanlong Du <legendu.net@outlook.com>
2021-03-20 16:12:40 -0700

    reduce keywords

--- dsutil/hadoop/log.py
+++ dsutil/hadoop/log.py
@@ -58,11 +58,7 @@ class LogFilter:
     """A class for log filtering.
     """
     KEYWORDS = (
-        "User class threw exception",
         "spark.yarn.executor.memoryOverhead",
-        "FileAlreadyExists",
-        "InvalidResourceRequestException",
-        "has no attribute",
         "not found",
         "OOM",
         "Error",
@@ -71,8 +67,8 @@ class LogFilter:
         "exception",
     )
     PATTERNS = (
-        r"\d\d[\/](0?[1-9]|1[0-2])[\/](0?[1-9]|[12][0-9]|3[01])\s\d+[:]\d+:\d+",
-        r"((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\.|$)){4}",
+        r"\d\d[\/](0?[1-9]|1[0-2])[\/](0?[1-9]|[12][0-9]|3[01])\s\d+[:]\d+:\d+",  # time
+        r"((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\.|$)){4}",  # IP
     )
 
     def __init__(
